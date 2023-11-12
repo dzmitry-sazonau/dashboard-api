3b97b5a96dfee9e3b12ef61b0b7bb33bf9b9487b
Dzmitry Sazonau <dzmitry.sazonau@gmail.com>
2023-11-12 19:40:17 +0100

    add users controller

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,28 @@
 import express, { Express } from "express";
 import { Server } from 'http'
 import { LoggerService } from "./logger/logger.service";
+import {UsersController} from "./users/users.controller";
 
 export class App {
   app: Express
   server: Server
   port: number;
   logger: LoggerService
+  userController: UsersController
 
-  constructor(logger: LoggerService) {
+  constructor(
+    logger: LoggerService,
+    userController: UsersController
+  ) {
     this.app = express();
     this.port = 8000;
     this.logger = logger;
+    this.userController = userController;
   }
 
   useRouter() {
-    // this.app.use('/users', {})
+    this.app.use('/users', this.userController.router)
   }
-
 
   public async init() {
     this.useRouter();
