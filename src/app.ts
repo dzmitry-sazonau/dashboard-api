2a024c2106aec1f0d44e52ac507918c39e979ed4
Dzmitry Sazonau <dzmitry.sazonau@gmail.com>
2023-11-12 19:02:14 +0100

    add logger service

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 import express, { Express } from "express";
 import { Server } from 'http'
+import { LoggerService } from "./logger/logger.service";
 
 export class App {
   app: Express
   server: Server
   port: number;
-  constructor() {
+  logger: LoggerService
+
+  constructor(logger: LoggerService) {
     this.app = express();
     this.port = 8000;
+    this.logger = logger;
   }
 
   useRouter() {
@@ -19,7 +23,7 @@ export class App {
     this.useRouter();
 
     this.server = this.app.listen(this.port, () => {
-      console.log(`Server running on http://localhost:${this.port}`)
+      this.logger.log(`Server running on http://localhost:${this.port}`)
     });
 
   }
